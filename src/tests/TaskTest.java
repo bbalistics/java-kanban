004e6dbf17c07a7448ced71da6ba01e5b6676e23
Vlad Nsv <mmm.kkkw@mail.ru>
2025-03-07 21:38:13 +0500

    new

--- src/tests/TaskTest.java
+++ src/tests/TaskTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TaskTest {
     @Test
-    public void areEqualTasksEqual(){
+    public void areEqualTasksEqual() {
         Task task1 = new Task("Продай гараж", "время до завтра", 1, Status.NEW);
         Task task2 = new Task("Купи гараж", "время до вчера", 1, Status.NEW);
 
